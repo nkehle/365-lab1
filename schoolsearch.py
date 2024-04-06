058bd6c075a5636aa702c0e154871b31fe157483
Noa Kehle <nkehle@calpoly.edu>
2024-04-05 17:23:10 -0700

    added main func

--- schoolsearch.py
+++ schoolsearch.py
@@ -1,1 +1,20 @@
+import students.txt
+
+def main():
+    # split into lines
+    file = students.txt
+    lines = file.split()
+
+    # put into data structure
+
+    user_input = ""
+    while(user_input != "Q"):
+        user_input = input("Please enter a command:")
+
+        # pass the data structure to the proper command
+
+
+if __name__ == "__main__":
+    main()
+
 
